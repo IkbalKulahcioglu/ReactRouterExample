eec2506a63435611107bad627b0633537e0f1f78
Ikbal Kulahcioglu <ikbalkulahcioglu@gmail.com>
2023-10-14 10:32:19 +0300

    Project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,45 @@
-import { useState } from 'react'
-import './App.css'
-import {Routes, Route} from 'react-router-dom'
-import Home from './components/Home'
-import AboutUs from './components/AboutUs'
-import Navbar from './components/Navbar'
-import Mission from './components/Mission'
-import WrongPath from './components/WrongPath'
-import Team from './components/Team'
-import Company from './components/Company'
-import History from './components/History'
-import Members from './components/Members'
-import MemberDetail from './components/MemberDetail'
-
+import React, { useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+// import AboutUs from './components/AboutUs'
+import Navbar from "./components/Navbar";
+import Mission from "./components/Mission";
+import WrongPath from "./components/WrongPath";
+import Team from "./components/Team";
+import Company from "./components/Company";
+import History from "./components/History";
+import Members from "./components/Members";
+import MemberDetail from "./components/MemberDetail";
+const LazyAbout = React.lazy(() => import("./components/AboutUs"));
 function App() {
-
   return (
     <>
-     <div className='App'>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/aboutUs' element={<AboutUs/>}/>
-        <Route path='/mission' element={<Mission/>}/>
-        <Route path='/member' element={<Members/>}/>
-        <Route path='/members' element={<Members/>}/>
-        <Route path='/members/:memberId' element={<MemberDetail/>}/>
-        <Route path='/history' element={<History/>}> 
-          <Route path='team' element={<Team  />}/>
-          <Route path='company' element={<Company />}/>
-        </Route>
-        <Route path='*' element={<WrongPath />}/>
-      </Routes>
-     </div>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/aboutUs"
+            element={
+              <React.Suspense>
+                <LazyAbout />
+              </React.Suspense>
+            }
+          />
+          <Route path="/mission" element={<Mission />} />
+          <Route path="/member" element={<Members />} />
+          <Route path="/members" element={<Members />} />
+          <Route path="/members/:memberId" element={<MemberDetail />} />
+          <Route path="/history" element={<History />}>
+            <Route path="team" element={<Team />} />
+            <Route path="company" element={<Company />} />
+          </Route>
+          <Route path="*" element={<WrongPath />} />
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 function AboutUs() {
   return (
-    <div>AboutUs</div>
-  )
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur sed
+      ipsa nihil mollitia, fugit quia vitae aut, saepe ea sit tempora eveniet
+      vero accusantium iure! Fuga placeat dolore commodi neque?
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur sed
+      ipsa nihil mollitia, fugit quia vitae aut, saepe ea sit tempora eveniet
+      vero accusantium iure! Fuga placeat dolore commodi neque?
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur sed
+      ipsa nihil mollitia, fugit quia vitae aut, saepe ea sit tempora eveniet
+      vero accusantium iure! Fuga placeat dolore commodi neque?
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur sed
+      ipsa nihil mollitia, fugit quia vitae aut, saepe ea sit tempora eveniet
+      vero accusantium iure! Fuga placeat dolore commodi neque?
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur sed
+      ipsa nihil mollitia, fugit quia vitae aut, saepe ea sit tempora eveniet
+      vero accusantium iure! Fuga placeat dolore commodi neque?
+    </div>
+  );
 }
 
-export default AboutUs
+export default AboutUs;
